4ace629797f132a2a979f9cca57c33ab1462aea3
Sharat <sharatchandrats@gmail.com>
2025-09-13 19:24:42 +0530

    Changed url to support render

--- backend/database/db.py
+++ backend/database/db.py
@@ -6,6 +6,9 @@ from dotenv import load_dotenv
 # Load environment variables from .env
 load_dotenv()
 
+# This is to run locally
+
+""" 
 # Setting default values to handle null errors
 DB_USER = os.getenv("DB_USER", "postgres")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "password")
@@ -14,9 +17,13 @@ DB_PORT = os.getenv("DB_PORT", "5432")   # default 5432
 DB_NAME = os.getenv("DB_NAME", "testdb")
 
 # Example: postgresql+psycopg2://postgres:your_password@localhost:5432/your_database_name
+
 DATABASE_URL = (
     f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-)
+) 
+"""
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 # Create SQLAlchemy engine
 engine = create_engine(DATABASE_URL)
